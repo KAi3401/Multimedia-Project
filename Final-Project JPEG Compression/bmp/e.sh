5a8dbfce5cb5758d70e37f23d4a572f22886022a
KAi3401 <127239001+KAi3401@users.noreply.github.com>
2024-01-07 13:13:15 +0800

    Delete Final-Project JPEG Compression/bmp directory

--- Final-Project JPEG Compression/bmp/e.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gcc encoder.c -o encoder -lm
-# ./encoder 0 Input2.bmp R.txt G.txt B.txt dim.txt
-./encoder 1 Input2.bmp Qt_Y.txt Qt_Cb.txt Qt_Cr.txt dim.txt qF_Y.raw qF_Cb.raw qF_Cr.raw eF_Y.raw eF_Cb.raw eF_Cr.raw
-echo "done"
-rm encoder.exe
